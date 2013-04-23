db5d02dc73c834a1285fe4649bdfc17a475a121f
Andrew Kelley <superjoe30@gmail.com>
2013-04-23 15:05:32 -0400

    create hello world bitcode

--- asm6502/compile.go
+++ asm6502/compile.go
@@ -2,6 +2,7 @@ package asm6502
 
 import (
 	"github.com/axw/gollvm/llvm"
+	"os"
 )
 
 func (p *Program) Compile(filename string) error {
@@ -10,17 +11,30 @@ func (p *Program) Compile(filename string) error {
 	defer builder.Dispose()
 
 	mod := llvm.NewModule("asm_module")
-	main_type := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{}, false)
-	main_fn := llvm.AddFunction(mod, "main", main_type)
-	main_fn.SetFunctionCallConv(llvm.CCallConv)
-	entry := llvm.AddBasicBlock(main_fn, "entry")
+	helloWorldText := llvm.ConstString("Hello, world!", true)
+	strGlobal := llvm.AddGlobal(mod, helloWorldText.Type(), "str")
+	strGlobal.SetLinkage(llvm.PrivateLinkage)
+	strGlobal.SetInitializer(helloWorldText)
+	strGlobal.SetGlobalConstant(true)
+	bytePointerType := llvm.PointerType(llvm.Int8Type(), 0)
+	putsType := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{bytePointerType}, false)
+	putsFn := llvm.AddFunction(mod, "puts", putsType)
+	putsFn.SetFunctionCallConv(llvm.CCallConv)
+	putsFn.SetLinkage(llvm.ExternalLinkage)
+	mainType := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{}, false)
+	mainFn := llvm.AddFunction(mod, "main", mainType)
+	mainFn.SetFunctionCallConv(llvm.CCallConv)
+	entry := llvm.AddBasicBlock(mainFn, "entry")
 	builder.SetInsertPointAtEnd(entry)
+	strPtr := builder.CreatePointerCast(strGlobal, bytePointerType, "")
+	builder.CreateCall(putsFn, []llvm.Value{strPtr}, "")
 	builder.CreateRet(llvm.ConstInt(llvm.Int32Type(), 0, false))
+
 
 	err := llvm.VerifyModule(mod, llvm.ReturnStatusAction)
 	if err != nil { return err }
 
-	engine, err := llvm.NewJITCompiler(mod, 2)
+	engine, err := llvm.NewJITCompiler(mod, 3)
 	if err != nil { return err }
 	defer engine.Dispose()
 
@@ -36,5 +50,14 @@ func (p *Program) Compile(filename string) error {
 	pass.Run(mod)
 
 	mod.Dump()
+
+	fd, err := os.Create(filename)
+	if err != nil { return err}
+
+	err = llvm.WriteBitcodeToFile(mod, fd)
+	if err != nil { return err }
+
+	err = fd.Close()
+	if err != nil { return err }
 	return nil
 }
