e3c83215b904448cc4b431ffdcb5a61f79727c1a
Andrew Kelley <superjoe30@gmail.com>
2013-04-25 19:29:07 -0400

    add zelda as part of automated tests

--- asm6502/asm6502_test.go
+++ asm6502/asm6502_test.go
@@ -4,18 +4,37 @@ import (
 	"testing"
 	"io/ioutil"
 	"bytes"
+	"fmt"
 )
 
-func TestSuite6502Asm(t *testing.T) {
-	expected, err := ioutil.ReadFile("test/suite6502.bin.ref")
-	if err != nil { t.Error(err) }
-	programAst, err := ParseFile("test/suite6502.asm")
-	if err != nil { t.Error(err) }
-	program := programAst.ToProgram()
-	if len(program.Errors) > 0 { t.Error("unexpected errors") }
-	buf := new(bytes.Buffer)
-	program.Assemble(buf)
-	if bytes.Compare(buf.Bytes(), expected) != 0 {
-		t.Error("does not match expected output")
+type testAsm struct {
+	inFile string
+	expectedOutFile string
+}
+
+var testAsmList = []testAsm{
+	{
+		"test/suite6502.asm",
+		"test/suite6502.bin.ref",
+	},
+	{
+		"test/zelda.asm",
+		"test/zelda.bin.ref",
+	},
+}
+
+func TestAsm(t *testing.T) {
+	for _, ta := range(testAsmList) {
+		expected, err := ioutil.ReadFile(ta.expectedOutFile)
+		if err != nil { t.Error(err) }
+		programAst, err := ParseFile(ta.inFile)
+		if err != nil { t.Error(err) }
+		program := programAst.ToProgram()
+		if len(program.Errors) > 0 { t.Error(fmt.Sprintf("%s: unexpected errors", ta.inFile)) }
+		buf := new(bytes.Buffer)
+		program.Assemble(buf)
+		if bytes.Compare(buf.Bytes(), expected) != 0 {
+			t.Error(fmt.Sprintf("%s: does not match expected output", ta.inFile))
+		}
 	}
 }
